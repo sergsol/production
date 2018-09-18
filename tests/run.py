5d651309d9dda170444579fb82b2fd136485347d
sergsol <serge.soloshchenko@gmail.com>
2018-09-18 10:58:30 +0200

    Added requirements txt Added tests for language

---
+++ tests/run.py
@@ -0,0 +1,22 @@
+import time
+import numpy as np
+import cv2
+
+
+
+
+expected = cv2.imread('C:\\Users\\ssoloshchenko\\job_control\\Expected\\PL.png')
+actual = cv2.imread('C:\\Users\\ssoloshchenko\\job_control\\2018-09-04_12-39\\PL.png')
+difference = cv2.subtract(expected, actual)
+result = not np.any(difference)
+if result is True:
+    print("Images are same")
+
+else:
+    cv2.imwrite('C:\\Users\\ssoloshchenko\\job_control\\2018-09-04_12-39\\script_result.png', difference)
+    print('Images are different')
+m = actual.shape
+n = actual.shape
+print(n)
+print(m)
+print(difference)
